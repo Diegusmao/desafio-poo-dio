a5b4f59274c3637de9a78898e2cec18821d960ac
Diego Gusmão <diegobgusmao@gmail.com>
2023-05-29 19:35:35 -0300

    Primeiro Commit, ajustando e organizando projeto

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,17 +1,11 @@
+package br.com.dio.desafio.dominio;
 
 
 import java.time.LocalDate;
 
-import br.com.dio.desafio.dominio.Bootcamp;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Dev;
-import br.com.dio.desafio.dominio.Mentoria;
-
 public class Main {
-   
-	public static void main(String[] args) {
-       
-		Curso curso1 = new Curso();
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
